aa1c9ba377b7b71a322f5687ea5fa2ffa6d26a2a
kgang <eichlefoko@gmail.com>
2014-04-20 23:28:49 +0200

    added a test method to test falling object factory

---
+++ Madrix/Madtrix.Tests/GivenFileLoggerFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Madtrix.Factories;
+using Madtrix.Factories.GameObjects;
+using NUnit.Framework;
+
+namespace Madtrix.Tests
+{
+    [TestFixture]
+    public class GivenFallingObjectFactory
+    {
+        [Test]
+        public void ThenReturnRaindropObject()
+        {
+            var fallingObjectFactory = new FallingObjectFactory();
+            var fallingGameObject = fallingObjectFactory.CreateGameObject();
+            Assert.IsInstanceOf(typeof(Raindrop), fallingGameObject, "The falling object factory did not return a raindrop");
+        }
+    }
+}
