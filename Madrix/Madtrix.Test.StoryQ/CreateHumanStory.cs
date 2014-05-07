c07794725ed28d1082701f849b9aef79753bdb5a
kgang <eichlefoko@gmail.com>
2014-05-07 18:50:15 +0200

    one of my projects was not in source control

---
+++ Madrix/Madtrix.Test.StoryQ/CreateHumanStory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Madtrix.Factories;
+using Madtrix.Factories.GameObjects;
+using NUnit.Framework;
+using StoryQ;
+
+namespace Madtrix.Test.StoryQ
+{
+    [TestFixture]
+    [Category("Create dodging Object")]
+    public class CreateHumanDropStory
+    {
+        IGameObjectFactory DodgingObjectFactory = null;
+        IGameObject DodgingGameObject = null;
+        [Test]
+        public void CreateFallingObject()
+        {
+            new Story("Create dodgingObject")
+                .InOrderTo("create a human")
+                .AsA("System")
+                .IWant("to create a human object")
+
+                        .WithScenario("Create a human")
+                            .Given(IHaveADodgingObjectFactory)
+                            .When(ICreateAHuman)
+                            .Then(TheResultShouldBeAHumanOnTheScreen)
+                .ExecuteWithReport();
+        }
+
+        private void IHaveADodgingObjectFactory()
+        {
+            DodgingObjectFactory = new DodgingObjectFactory();
+
+
+        }
+
+        private void ICreateAHuman()
+        {
+            DodgingGameObject = DodgingObjectFactory.CreateGameObject((int)ObjectType.Human);
+
+        }
+
+        private void TheResultShouldBeAHumanOnTheScreen()
+        {
+            Assert.IsInstanceOf(typeof(Human), DodgingGameObject, "The dodging object factory did not return a raindrop");
+        }
+    }
+}
---
+++ Madrix/Madtrix.Test.StoryQ/CreateRainDropStory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Madtrix.Factories;
+using Madtrix.Factories.GameObjects;
+using NUnit.Framework;
+using StoryQ;
+
+namespace Madtrix.Test.StoryQ
+{
+    [TestFixture]
+    [Category("Create Falling Object")]
+    public class CreateRainDropStory
+    {
+        IGameObjectFactory fallingObjectFactory = null;
+        IGameObject fallingGameObject = null;
+        [Test]
+        public void CreateFallingObject()
+        {
+            new Story("Create FallingObject")
+                .InOrderTo("create rain drops")
+                .AsA("System")
+                .IWant("to create a falling object")
+
+                        .WithScenario("Create a raindrop")
+                            .Given(IHaveAFallingObjectFactory)
+                            .When(ICreateARaindrop)
+                            .Then(TheResultShouldBeARainDropOnTheScreen)
+                .ExecuteWithReport();
+        }
+
+        private void IHaveAFallingObjectFactory()
+        {
+            fallingObjectFactory = new FallingObjectFactory();
+            
+            
+        }
+
+        private void ICreateARaindrop()
+        {
+            fallingGameObject = fallingObjectFactory.CreateGameObject((int)ObjectType.Raindrop);
+            
+        }
+
+        private void TheResultShouldBeARainDropOnTheScreen()
+        {
+            Assert.IsInstanceOf(typeof(Raindrop), fallingGameObject, "The falling object factory did not return a raindrop");
+        }
+    }
+}
